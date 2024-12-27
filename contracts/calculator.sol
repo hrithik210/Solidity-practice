c8c3e4aeba0a2bc1c233a99381c7f64446581d63
hrithik210 <hrithiknayak7829@gmail.com>
2024-12-27 20:45:32 +0530

    wrote a simple calculator contract

--- contracts/calculator.sol
+++ contracts/calculator.sol
@@ -12,6 +12,18 @@ contract Calculator{
        return nums+=_nums ;
     }
 
+    function subtract(uint _nums) public returns (uint){
+        return nums-=_nums ;
+    }
+
+    function multipy(uint _nums) public returns (uint){
+        return nums*=_nums;
+    }
+
+    function divide(uint _nums) public returns (uint){
+        require(nums !=0 , "cannot divide by 0");
+        return nums/=_nums;
+    }
     function getValue() public view returns (uint) {
         return nums;
     }
