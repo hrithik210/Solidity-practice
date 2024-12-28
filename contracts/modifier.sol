0a79148d010c06a3eac525e610b5c9e47deca9b4
hrithik210 <hrithiknayak7829@gmail.com>
2024-12-29 00:07:24 +0530

    fixed access modifier

--- contracts/modifier.sol
+++ contracts/modifier.sol
@@ -3,20 +3,24 @@ pragma solidity >=0.7.0 < 0.9.0 ;
 
 contract PracticeModifiers{
 
-    uint owner ;
+    address owner ;
     uint num ;
     constructor(uint _nums){
         owner = msg.sender ;
-        nums = _nums;
+        num = _nums;
     
     }
     modifier isOwner{
-        require(owner == msg.sender);
+        require(owner == msg.sender, "u ain the owner lil bro");
         _;
     }
 
 
-    function add() public returns (uint) isOwner {
+    function add(uint _nums) public isOwner returns (uint)  {
+        return num += _nums ;
+    }
 
+    function getValue() public view returns (uint) {
+        return num ;
     }
 }
